307b434190338230056e2d77ba938bcd09613758
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2022-01-12 02:08:55 +0300

    update stats.py

--- Yukki/Inline/stats.py
+++ Yukki/Inline/stats.py
@@ -3,84 +3,84 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 stats1 = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="System Stats", callback_data=f"sys_stats"),
-            InlineKeyboardButton(text="Storage Stats", callback_data=f"sto_stats"),
+            InlineKeyboardButton(text="Sistem İstatistikleri", callback_data=f"sys_stats"),
+            InlineKeyboardButton(text="Depolama İstatistikleri", callback_data=f"sto_stats"),
         ],
         [
-            InlineKeyboardButton(text="Bot Stats", callback_data=f"bot_stats"),
-            InlineKeyboardButton(text="MongoDB Stats", callback_data=f"mongo_stats"),
+            InlineKeyboardButton(text="Bot İstatistikleri", callback_data=f"bot_stats"),
+            InlineKeyboardButton(text="MongoDB İstatistikleri", callback_data=f"mongo_stats"),
         ],
-        [InlineKeyboardButton(text="Assistant Stats", callback_data=f"assis_stats")],
+        [InlineKeyboardButton(text="Asistan İstatistikleri", callback_data=f"assis_stats")],
     ]
 )
 
 stats2 = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="General Stats", callback_data=f"gen_stats"),
-            InlineKeyboardButton(text="Storage Stats", callback_data=f"sto_stats"),
+            InlineKeyboardButton(text="Genel İstatistikler", callback_data=f"gen_stats"),
+            InlineKeyboardButton(text="Depolama İstatistikleri", callback_data=f"sto_stats"),
         ],
         [
-            InlineKeyboardButton(text="Bot Stats", callback_data=f"bot_stats"),
-            InlineKeyboardButton(text="MongoDB Stats", callback_data=f"mongo_stats"),
+            InlineKeyboardButton(text="Bot İstatistikleri", callback_data=f"bot_stats"),
+            InlineKeyboardButton(text="MongoDB İstatistikleri", callback_data=f"mongo_stats"),
         ],
-        [InlineKeyboardButton(text="Assistant Stats", callback_data=f"assis_stats")],
+        [InlineKeyboardButton(text="Asistan İstatistikleri", callback_data=f"assis_stats")],
     ]
 )
 
 stats3 = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="System Stats", callback_data=f"sys_stats"),
-            InlineKeyboardButton(text="General Stats", callback_data=f"gen_stats"),
+            InlineKeyboardButton(text="Sistem İstatistikleri", callback_data=f"sys_stats"),
+            InlineKeyboardButton(text="Genel İstatistikler", callback_data=f"gen_stats"),
         ],
         [
-            InlineKeyboardButton(text="Bot Stats", callback_data=f"bot_stats"),
-            InlineKeyboardButton(text="MongoDB Stats", callback_data=f"mongo_stats"),
+            InlineKeyboardButton(text="Bot İstatistikleri", callback_data=f"bot_stats"),
+            InlineKeyboardButton(text="MongoDB İstatistikleri", callback_data=f"mongo_stats"),
         ],
-        [InlineKeyboardButton(text="Assistant Stats", callback_data=f"assis_stats")],
+        [InlineKeyboardButton(text="Asistan İstatistikleri", callback_data=f"assis_stats")],
     ]
 )
 
 stats4 = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="System Stats", callback_data=f"sys_stats"),
-            InlineKeyboardButton(text="Storage Stats", callback_data=f"sto_stats"),
+            InlineKeyboardButton(text="Sistem İstatistikleri", callback_data=f"sys_stats"),
+            InlineKeyboardButton(text="Depolama İstatistikleri", callback_data=f"sto_stats"),
         ],
         [
-            InlineKeyboardButton(text="General Stats", callback_data=f"gen_stats"),
-            InlineKeyboardButton(text="MongoDB Stats", callback_data=f"mongo_stats"),
+            InlineKeyboardButton(text="Genel İstatistikler", callback_data=f"gen_stats"),
+            InlineKeyboardButton(text="MongoDB İstatistikleri", callback_data=f"mongo_stats"),
         ],
-        [InlineKeyboardButton(text="Assistant Stats", callback_data=f"assis_stats")],
+        [InlineKeyboardButton(text="Asistant İstatistikleri", callback_data=f"assis_stats")],
     ]
 )
 
 stats5 = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="System Stats", callback_data=f"sys_stats"),
-            InlineKeyboardButton(text="Storage Stats", callback_data=f"sto_stats"),
+            InlineKeyboardButton(text="Sistem İstatistikleri", callback_data=f"sys_stats"),
+            InlineKeyboardButton(text="Depolama İstatistikleri", callback_data=f"sto_stats"),
         ],
         [
-            InlineKeyboardButton(text="Bot Stats", callback_data=f"bot_stats"),
-            InlineKeyboardButton(text="General Stats", callback_data=f"gen_stats"),
+            InlineKeyboardButton(text="Bot İstatistikleri", callback_data=f"bot_stats"),
+            InlineKeyboardButton(text="Genel İstatistikleri", callback_data=f"gen_stats"),
         ],
-        [InlineKeyboardButton(text="Assistant Stats", callback_data=f"assis_stats")],
+        [InlineKeyboardButton(text="Asistan İstatistikleri", callback_data=f"assis_stats")],
     ]
 )
 
 stats6 = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="System Stats", callback_data=f"sys_stats"),
-            InlineKeyboardButton(text="Storage Stats", callback_data=f"sto_stats"),
+            InlineKeyboardButton(text="Sistem İstatistikleri", callback_data=f"sys_stats"),
+            InlineKeyboardButton(text="Depolama İstatistikleri", callback_data=f"sto_stats"),
         ],
         [
-            InlineKeyboardButton(text="Bot Stats", callback_data=f"bot_stats"),
-            InlineKeyboardButton(text="MongoDB Stats", callback_data=f"mongo_stats"),
+            InlineKeyboardButton(text="Bot İstatistikleri", callback_data=f"bot_stats"),
+            InlineKeyboardButton(text="MongoDB İstatistikleri", callback_data=f"mongo_stats"),
         ],
-        [InlineKeyboardButton(text="General Stats", callback_data=f"gen_stats")],
+        [InlineKeyboardButton(text="Genel İstatistikler", callback_data=f"gen_stats")],
     ]
 )
 
@@ -89,7 +89,7 @@ stats7 = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                text="Getting Assistant Stats....",
+                text="Asistan İstatistiklerini Alma...",
                 callback_data=f"wait_stats",
             )
         ]
