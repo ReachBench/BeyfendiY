96b3c5037e970bf73b99a05c6a110b0b86b931cb
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2022-01-12 11:27:01 +0300

    update Assistant.py

--- Yukki/Plugins/Assistant.py
+++ Yukki/Plugins/Assistant.py
@@ -6,49 +6,49 @@ from pyrogram.types import Message
 from Yukki import ASSISTANT_PREFIX, SUDOERS, app, random_assistant
 from Yukki.Database import get_assistant, save_assistant
 
-__MODULE__ = "Assistant"
+__MODULE__ = "Asistan"
 __HELP__ = f"""
 
 
-/checkassistant
-- Check the alloted assistant of your chat
+/asistankontrol
+- Sohbetinizde kullanılan asistanı kontrol edin
 
 
-**Note:**
-- Only for Sudo Users
+**Not:**
+- Sadece Kurucular İçin
 
-{ASSISTANT_PREFIX[0]}block [ Reply to a User Message] 
-- Blocks the User from Assistant Account.
+{ASSISTANT_PREFIX[0]} Engelle [Bir Kullanıcı Mesajını Yanıtla]
+- Kullanıcıyı Asistan Hesabından Engeller.
 
-{ASSISTANT_PREFIX[0]}unblock [ Reply to a User Message] 
-- Unblocks the User from Assistant Account.
+{ASSISTANT_PREFIX[0]} Engeli Kaldır [Bir Kullanıcı Mesajına yanıt verme]
+- Kullanıcının Asistan Hesabındaki engellemesini kaldırır.
 
-{ASSISTANT_PREFIX[0]}approve [ Reply to a User Message] 
-- Approves the User for DM.
+{ASSISTANT_PREFIX[0]} Onayla [Bir Kullanıcı Mesajını Yanıtla]
+- Kullanıcıyı DM için onaylar.
 
-{ASSISTANT_PREFIX[0]}disapprove [ Reply to a User Message] 
-- Disapproves the User for DM.
+{ASSISTANT_PREFIX[0]} Onaylama [Bir Kullanıcı Mesajına Cevap Ver]
+- Kullanıcıyı DM için onaylamaz.
 
-{ASSISTANT_PREFIX[0]}pfp [ Reply to a Photo] 
-- Changes Assistant account PFP.
+{ASSISTANT_PREFIX[0]} Profil Fotoğrafı [ Bir Fotoğrafa Yanıt Ver]
+- Asistan hesabı Profil Fotoğrafını değiştirir.
 
-{ASSISTANT_PREFIX[0]}bio [Bio text] 
-- Changes Bio of Assistant Account.
+{ASSISTANT_PREFIX[0]} bio [Bio Mesajı] 
+- Asistan Hesabın Bio Yazısını Değiştirir.
 
-/changeassistant [ASS NUMBER]
-- Change the previoius alloted assistant to new one.
+/asistandegistir [1 den 5 e kadar olan numara]
+- Asistan Hesabın Hangisi olduğunu gösterir.
 
-/setassistant [ASS NUMBER or Random]
-- Set a assistant account for chat. 
+/asistanayarla [bu komutu tek başına kullanırsanız rastgele bir asistan seçer veya 1 den 5 e kadar olan bir numara girerek istediğiniz asistanı ayarlayabilirsiniz]
+- Sohbet için bir asistan hesabı ayarlayın.
 """
 
 
 ass_num_list = ["1", "2", "3", "4", "5"]
 
 
-@app.on_message(filters.command("changeassistant") & filters.user(SUDOERS))
+@app.on_message(filters.command("asistandegistir") & filters.user(SUDOERS))
 async def assis_change(_, message: Message):
-    usage = f"**Usage:**\n/changeassistant [ASS_NO]\n\nSelect from them\n{' | '.join(ass_num_list)}"
+    usage = f"**Kullanım:** \n/asistandegistir [Asistan Numarası] \n\nAralarından seçim yapın \n{' | '.join(ass_num_list)}"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     num = message.text.split(None, 1)[1].strip()
@@ -58,7 +58,7 @@ async def assis_change(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /setassistant"
+            "Önceden Kaydedilmiş Asistan Bulunamadı. \n\nAsistanı /asistanayarla komutu ile ayarlayabilirsiniz"
         )
     else:
         ass = _assistant["saveassistant"]
@@ -67,16 +67,16 @@ async def assis_change(_, message: Message):
     }
     await save_assistant(message.chat.id, "assistant", assis)
     await message.reply_text(
-        f"**Changed Assistant**\n\nChanged Assistant Account from **{ass}** to Assistant Number **{ass_num}**"
+        f"**Asistan Değiştirildi**\n\nAsistan hesabı şundan **{ass}** Şuna değiştirildi **{ass_num}**"
     )
 
 
 ass_num_list2 = ["1", "2", "3", "4", "5", "Random"]
 
 
-@app.on_message(filters.command("setassistant") & filters.user(SUDOERS))
+@app.on_message(filters.command("asistanayarla") & filters.user(SUDOERS))
 async def assis_change(_, message: Message):
-    usage = f"**Usage:**\n/setassistant [ASS_NO or Random]\n\nSelect from them\n{' | '.join(ass_num_list2)}\n\nUse 'Random' to set random Assistant"
+    usage = f"**Kullanım:**\n/asistanayarla [Asistan Numarası Veya Tek başına Komut Kullanımı] \n\nAralarından Seçim Yapın \n{' | '.join(ass_num_list2)} \n\nRastgele Asistan Ayarlamak İçin Komutu Tek Başına Kullanın"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     query = message.text.split(None, 1)[1].strip()
@@ -89,7 +89,7 @@ async def assis_change(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         await message.reply_text(
-            f"**__Yukki Music Bot Assistant Alloted__**\n\nAssistant No. **{ran_ass}**"
+            f"**Her Telden Müzik Botu Asistanı Atandı** \n\nAsistan Numarası: **{ran_ass}**"
         )
         assis = {
             "saveassistant": ran_ass,
@@ -98,19 +98,19 @@ async def assis_change(_, message: Message):
     else:
         ass = _assistant["saveassistant"]
         return await message.reply_text(
-            f"Pre-Saved Assistant Number {ass} Found.\n\nYou can change Assistant Via /changeassistant"
+            f"Önceden Kaydedilmiş Asistan Numarası {ass} Bulundu. \n\nAsistan Değiştirmek İçin Komut: /asistandegistir"
         )
 
 
-@app.on_message(filters.command("checkassistant") & filters.group)
+@app.on_message(filters.command("asistankontrol") & filters.group)
 async def check_ass(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /play"
+            "Önceden Kaydedilmiş Asistan Bulunamadı. \n\nAsistanı ayarlamak için komut: /asistanayarla"
         )
     else:
         ass = _assistant["saveassistant"]
         return await message.reply_text(
-            f"Pre-Saved Assistant Found\n\nAssistanty Number {ass} "
+            f"Önceden Kaydedilmiş Asistan Bulundu. \n\nAsistan Numarası: {ass} "
         )
