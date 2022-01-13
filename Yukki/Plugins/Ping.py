21fbda0f11824d075c8db77b38637f2a18034661
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2022-01-14 02:30:48 +0300

    update Ping.py

--- Yukki/Plugins/Ping.py
+++ Yukki/Plugins/Ping.py
@@ -10,7 +10,7 @@ from Yukki.Utilities.ping import get_readable_time
 __MODULE__ = "Ping"
 __HELP__ = """
 
-/ping - Check if Bot is alive or not.
+/ping - Botun hayatta olup olmadığını kontrol edin.
 """
 
 
@@ -20,10 +20,10 @@ async def bot_sys_stats():
     mem = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
     stats = f"""
-Uptime: {get_readable_time((bot_uptime))}
+çalışma süresi: {get_readable_time((bot_uptime))}
 CPU: {cpu}%
 RAM: {mem}%
-Disk: {disk}%"""
+Disk: %{disk}"""
     return stats
 
 
@@ -38,5 +38,5 @@ async def ping(_, message):
     end = datetime.now()
     resp = (end - start).microseconds / 1000
     await response.edit_text(
-        f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} System Stats:</u></b>{uptime}"
+        f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} Sistem İstatistikleri:</u></b>{uptime}"
     )
