75e9a77c1e7fbaefd064913bf78dc386108906fb
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2022-01-17 19:01:50 +0300

    update Speedtest.py

--- Yukki/Plugins/Speedtest.py
+++ Yukki/Plugins/Speedtest.py
@@ -6,43 +6,43 @@ from pyrogram import filters
 
 from Yukki import app
 
-__MODULE__ = "Speedtest"
+__MODULE__ = "Hız Testi"
 __HELP__ = """
 
-/speedtest 
-- Check Server Latency and Speed.
+/hiztest
+- Sunucu Gecikmesini ve Hızını Kontrol Edin.
 
 """
 
 
-@app.on_message(filters.command("speedtest") & ~filters.edited)
+@app.on_message(filters.command("hiztest") & ~filters.edited)
 async def statsguwid(_, message):
-    m = await message.reply_text("Running Speed test")
+    m = await message.reply_text("Hız Testi Başlatılıyor...")
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = await m.edit("Running Download SpeedTest")
+        m = await m.edit("İndirme Hızı Alınıyor...")
         test.download()
-        m = await m.edit("Running Upload SpeedTest")
+        m = await m.edit("Yükleme Hızı Alınıyor...")
         test.upload()
         test.results.share()
         result = test.results.dict()
     except Exception as e:
         return await m.edit(e)
-    m = await m.edit("Sharing SpeedTest Results")
+    m = await m.edit("Hız Testi Sonuçları Gönderiliyor...")
     path = wget.download(result["share"])
 
-    output = f"""**Speedtest Results**
+    output = f"""**Hız Testi Sonuçları**
     
-<u>**Client:**</u>
+<u>**Site Sunucusu:**</u>
 **__ISP:__** {result['client']['isp']}
-**__Country:__** {result['client']['country']}
+**__Ülke:__** {result['client']['country']}
   
-<u>**Server:**</u>
-**__Name:__** {result['server']['name']}
-**__Country:__** {result['server']['country']}, {result['server']['cc']}
+<u>**Bot Sunucusu:**</u>
+**__İsim:__** {result['server']['name']}
+**__Ülke:__** {result['server']['country']}, {result['server']['cc']}
 **__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
+**__Gecikme:__** {result['server']['latency']}  
 **__Ping:__** {result['ping']}"""
     msg = await app.send_photo(chat_id=message.chat.id, photo=path, caption=output)
     os.remove(path)
